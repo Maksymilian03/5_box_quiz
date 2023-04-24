470673c6ecdaca5cd3e85063c8aa8ea1826c30e8
PaulinaSko <PaulinaSko@users.noreply.github.com>
2023-04-24 20:52:33 +0200

    Changed paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import CustomLoginView, RegisterView, profile, ChangePasswordView
 
-# from .views import GeneratePdf
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
@@ -10,6 +9,4 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', profile, name='profile'),
     path('password-change/', ChangePasswordView.as_view(), name='password_change'),
-
-    # path('pdf/', GeneratePdf.as_view(), name='certificate'),
 ]
